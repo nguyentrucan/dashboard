a2ed4b9aea69149e2039e719888c6de6ac862079
nguyentrucan <an.ngtruc2002@gmail.com>
2025-03-12 12:02:16 +0700

    Create Route And Login Register Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import React, { lazy } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const App = lazy(() => import("./App.jsx"));
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
