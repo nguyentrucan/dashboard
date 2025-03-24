e63be001b820baf7e7fb880b5a1d8bf4c8db11f2
nguyentrucan <an.ngtruc2002@gmail.com>
2025-03-24 17:34:44 +0700

    Manage Project Navigation Design 1

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <div>
-            Sidebar
+  return (
+    <div>
+      <div className=""></div>
+      <div
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+      >
+        <div className="h-[70px] flex justify-center items-center">
+          <Link to="/" className="w-[180px] h-[50px]">
+            <img className="w-full h-full" src="http://localhost:5173/images/logo.png" alt="" />
+          </Link>
         </div>
-    );
+        <div className="px-[16px]">
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
