842af6d9957821c0a2f2d47a9857b115fb8220ba
nguyentrucan <an.ngtruc2002@gmail.com>
2025-04-15 16:56:56 +0700

    fix sidebar

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -52,7 +52,7 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
               </li>
             ))}
             <li>
-              <button className="text-[030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
+              <button className="text-[#030811] font-bold duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
                 <span>
                   <BiLogOutCircle />
                 </span>
