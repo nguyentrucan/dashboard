863ccf1d74ff07dce039a4e5966cfe4f31df48f2
nguyentrucan <an.ngtruc2002@gmail.com>
2025-04-17 10:09:33 +0700

    Category Page Design

--- src/views/admin/Category.jsx
+++ src/views/admin/Category.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
+import { FaEdit, FaImage, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Pagination from "../Pagination";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const Category = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -8,8 +11,17 @@ const Category = () => {
   const [show, setShow] = useState(false);
   return (
     <div className="px-2 lg:px-7 pt-5">
+      <div className="flex lg:hidden justify-between items-center mb-5 p-4 bg-[#6a5fdf] rounded-md">
+        <h1 className="text-[#d0d2d6] font-semibold text-lg">Category</h1>
+        <button
+          onClick={() => setShow(true)}
+          className="bg-red-500 shadow-lg hover:shadow-red-500/40 px-4 py-2 cursor-pointer text-white rounded-md text-sm"
+        >
+          Add
+        </button>
+      </div>
       <div className="flex flex-wrap w-full">
-        {/* 1 */}
+        {/* Category */}
         <div className="w-full lg:w-7/12">
           <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
             <div className="flex justify-between items-center">
@@ -50,41 +62,112 @@ const Category = () => {
                     <tr key={i}>
                       <td
                         scope="row"
-                        className="py-3 px-4 font-medium whitespace-nowrap"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
                       >
-                        #34344
+                        {d}
                       </td>
                       <td
                         scope="row"
-                        className="py-3 px-4 font-medium whitespace-nowrap"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
                       >
-                        $223
+                        <img
+                          className="w-[45px] h-[45px]"
+                          src={`/images/category/${d}.jpg`}
+                          alt=""
+                        />
                       </td>
                       <td
                         scope="row"
-                        className="py-3 px-4 font-medium whitespace-nowrap"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
                       >
-                        Pending
+                        T-Shirt
                       </td>
                       <td
                         scope="row"
-                        className="py-3 px-4 font-medium whitespace-nowrap"
+                        className="py-1 px-4 font-medium whitespace-nowrap"
                       >
-                        <Link>View</Link>
+                        <div className="flex justify-start items-center gap-4">
+                          <Link className="p-[6px] bg-yellow-500 rounded hover:shadow-lg hover:shadow-yellow-500">
+                            <FaEdit />
+                          </Link>
+                          <Link className="p-[6px] bg-red-500 rounded hover:shadow-lg hover:shadow-red-500">
+                            <FaTrash />
+                          </Link>
+                        </div>
                       </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
+            {/* Pagination */}
+            <div className="w-full flex justify-end mt-4 bottom-4 right-4">
+              <Pagination
+                pageNumber={currentPage}
+                setPageNumber={setCurrentPage}
+                totalItem={50}
+                parPage={parPage}
+                showItem={3}
+              />
+            </div>
+            {/* Pagination End */}
           </div>
         </div>
-        {/* 1 End */}
-        {/* 2 */}
-        <div className="w-full lg:w-5/12">
-          <div className="w-full p-4 bg-[#6a5fdf] rounded-md"></div>
+        {/* Category End */}
+        {/* Add Category */}
+        <div
+          className={`w-[320px] lg:w-5/12 lg:relative lg:right-0 fixed ${
+            show ? "right-0" : "-right-[340px]"
+          } z-[9999] top-0 transition-all duration-500`}
+        >
+          <div className="w-full pl-5 ">
+            <div className="bg-[#6a5fdf] rounded-md h-screen lg:h-auto px-3 py-2 lg:rounded-md text-[#d0d2d6]">
+              <div className="flex justify-between items-center mb-4">
+                <h1 className="text-[#d0d2d6] font-semibold text-xl mb-4 w-full text-center">
+                  Add Category
+                </h1>
+                <div onClick={() => setShow(false)} className="block lg:hidden">
+                  <IoMdCloseCircle />
+                </div>
+              </div>
+              <form>
+                <div className="flex flex-col w-full gap-1 mb-3">
+                  <label htmlFor="name">Category Name</label>
+                  <input
+                    className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#ffffff] border border-slate-700 rounded-md text-[#000000]"
+                    type="text"
+                    id="name"
+                    name="category_name"
+                    placeholder="Category Name"
+                  />
+                </div>
+                <div>
+                  <label
+                    className="flex justify-center items-center flex-col h-[238px] cursor-pointer border border-dashed hover:border-red-500 w-full border-[#d0d2d6]"
+                    htmlFor="image"
+                  >
+                    <span>
+                      <FaImage />
+                    </span>
+                    <span>Select Image</span>
+                  </label>
+                  <input
+                    className="hidden"
+                    type="file"
+                    name="image"
+                    id="image"
+                  />
+                  <div>
+                    <button className="bg-red-500 w-full hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2">
+                      Add Category
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        {/* 2 End */}
+        {/* Add Category End */}
       </div>
     </div>
   );
