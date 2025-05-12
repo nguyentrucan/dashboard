7836c63c4963544eeb768ae7b7dce679c913b47e
nguyentrucan <an.ngtruc2002@gmail.com>
2025-05-12 12:01:20 +0700

    Orders And Payments Page Design

--- src/router/routes/sellerRoutes.jsx
+++ src/router/routes/sellerRoutes.jsx
@@ -1,5 +1,7 @@
 import AddProduct from "../../views/seller/AddProduct";
 import DiscountProducts from "../../views/seller/DiscountProducts";
+import Orders from "../../views/seller/Orders";
+import Payments from "../../views/seller/Payments";
 import Products from "../../views/seller/Products";
 import SellerDashboard from "../../views/seller/SellerDashboard";
 import Home from "./../../views/Home";
@@ -13,21 +15,37 @@ export const sellerRoutes = [
   {
     path: "/seller/dashboard",
     element: <SellerDashboard />,
-    ability: ["seller"],
+    role: "seller",
+    status: "active",
   },
   {
     path: "/seller/dashboard/add-product",
     element: <AddProduct />,
-    ability: ["seller"],
+    role: "seller",
+    status: "active",
   },
   {
     path: "/seller/dashboard/all-products",
     element: <Products />,
-    ability: ["seller"],
+    role: "seller",
+    status: "active",
   },
   {
     path: "/seller/dashboard/discount-product",
     element: <DiscountProducts />,
-    ability: ["seller"],
+    role: "seller",
+    status: "active",
+  },
+  {
+    path: "/seller/dashboard/orders",
+    element: <Orders />,
+    role: "seller",
+    ability: ["active", "deactive"],
+  },
+  {
+    path: "/seller/dashboard/payments",
+    element: <Payments />,
+    role: "seller",
+    status: "active",
   },
 ];
